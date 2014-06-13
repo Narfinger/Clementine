8447433c52bcb44878fccd5154f4aeb110fdf230
Narfinger <Narfinger@users.noreply.github.com>
2014-06-14 00:27:10 +0200

    merge conflict

--- src/playlist/playlist.h
+++ src/playlist/playlist.h
@@ -83,10 +83,6 @@ class Playlist : public QAbstractListModel {
   ~Playlist();
 
   QElapsedTimer timer;
-<<<<<<< HEAD
-=======
-
->>>>>>> better timing
   void SkipTracks(const QModelIndexList& source_indexes);
 
   // Always add new columns to the end of this enum - the values are persisted
